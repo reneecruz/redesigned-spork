a6a1635bdbdb21033634d1c4a31ff28b5ff7a25f
Renée Cruz <reneebasscruz@gmail.com>
2020-03-17 11:25:40 -0400

    added button to link to external site, css for button, css for list style emoji

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={"https://assets.atlasobscura.com/media/W1siZnUiLCJodHRwczovL2F0bGFzLWRldi5zMy5hbWF6b25hd3MuY29tL21pc2MvbG9nb3MvT25lK0xpbmUrKENvbW1vbikvQXRsYXMrT2JzY3VyYStMb2dvK09uZStMaW5lK0NNWUsucG5nIl0sWyJwIiwiY29udmVydCIsIi1xdWFsaXR5IDgxIC1zdHJpcCJdXQ/Atlas%2BObscura%2BLogo%2BOne%2BLine%2BCMYK.png"} className="App-logo" alt="logo" />
         <h1>
-          Atlas Obscura Tribute
+          Atlas Obscura Tribute 🖤
         </h1>
         <Places />
       </header>
--- src/Places.js
+++ src/Places.js
@@ -22,8 +22,8 @@ useEffect(() => {
   const placesList = places.map(
     (p, id) => 
             <>
-            <img src={"https://assets.atlasobscura.com/media/W1siZiIsInVwbG9hZHMvcGxhY2VfaW1hZ2VzLzJjNzliZDJkLWMzZDEtNDJkMy04YzRiLWIzZDM1NGRmN2JjNjMwYjVlYWU1YzJhYTI4NTM5M19naG9zdF9yYW5jaF8xLmpwZyJdLFsicCIsInRodW1iIiwiMTIwMHg-Il0sWyJwIiwiY29udmVydCIsIi1xdWFsaXR5IDgxIC1hdXRvLW9yaWVudCJdXQ/ghost_ranch_4.jpg"} alt={p.lat} height="400" width="400" />
-            <li key={id}>Latitude: {p.lat} Longitude: {p.lon} Url: <a href={`https://atlasobscura.com/places/${p.id}`} target="blank">Atlas Obscura Entry</a>
+            <img src={"https://assets.atlasobscura.com/media/W1siZiIsInVwbG9hZHMvcGxhY2VfaW1hZ2VzLzJjNzliZDJkLWMzZDEtNDJkMy04YzRiLWIzZDM1NGRmN2JjNjMwYjVlYWU1YzJhYTI4NTM5M19naG9zdF9yYW5jaF8xLmpwZyJdLFsicCIsInRodW1iIiwiMTIwMHg-Il0sWyJwIiwiY29udmVydCIsIi1xdWFsaXR5IDgxIC1hdXRvLW9yaWVudCJdXQ/ghost_ranch_4.jpg"} alt={p.lat} height="777" width="777" />
+            <li key={id}>Latitude: {p.lat} Longitude: {p.lon} Url: <a href={`https://atlasobscura.com/places/${p.id}`} class="btn cta bg"target="blank">Atlas Obscura Entry</a>
             </li>
             </>
   );
