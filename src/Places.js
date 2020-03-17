14eb52803af0c142a0669e7df00cf8cc1968ef8d
Renée Cruz <reneebasscruz@gmail.com>
2020-03-16 23:30:54 -0400

    fetched to rails api backend

--- src/Places.js
+++ src/Places.js
@@ -10,7 +10,7 @@ function Places() {
 useEffect(() => {
     async function fetchData() {
       // You can await here
-      const response = await fetch('http://localhost:3001/Places');
+      const response = await fetch('http://localhost:3000/places');
       // ...
       const data = await response.json();
       setPlaces(data)
@@ -22,13 +22,11 @@ useEffect(() => {
   const placesList = places.map(
     (p, id) => 
             <>
-            <img src={"https://assets.atlasobscura.com/media/W1siZiIsInVwbG9hZHMvcGxhY2VfaW1hZ2VzLzFkMzlmZjBkLTc2OGQtNGZlMi05NzgzLWMzMDU2NDU4MWEyOTUwOWExYzc2MDlhNGUxMWYyNF9QXzIwMjAwMjIzXzE1NDEyNi5qcGciXSxbInAiLCJ0aHVtYiIsIjEyMDB4PiJdLFsicCIsImNvbnZlcnQiLCItcXVhbGl0eSA4MSAtYXV0by1vcmllbnQiXV0/P_20200223_154126.jpg"} alt={p.lat} height="400" width="400" />
-
-            <li key={id}>Latitude: {p.lat} Longitude: {p.lon}
+            <img src={"https://assets.atlasobscura.com/media/W1siZiIsInVwbG9hZHMvcGxhY2VfaW1hZ2VzLzJjNzliZDJkLWMzZDEtNDJkMy04YzRiLWIzZDM1NGRmN2JjNjMwYjVlYWU1YzJhYTI4NTM5M19naG9zdF9yYW5jaF8xLmpwZyJdLFsicCIsInRodW1iIiwiMTIwMHg-Il0sWyJwIiwiY29udmVydCIsIi1xdWFsaXR5IDgxIC1hdXRvLW9yaWVudCJdXQ/ghost_ranch_4.jpg"} alt={p.lat} height="400" width="400" />
+            <li key={id}>Latitude: {p.lat} Longitude: {p.lon} Url: <a href={`https://atlasobscura.com/places/${p.id}`} target="blank">Atlas Obscura Entry</a>
             </li>
             </>
   );
-
       useEffect(() => {
       return function logWillUnmount() {
         console.log("Unmounting place component");
