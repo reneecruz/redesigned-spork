543d7d493529ca7f00223907aa73ae76e5fd8836
Renée Cruz <reneebasscruz@gmail.com>
2020-03-17 16:31:22 -0400

    rename longitude key in api call map

--- src/Places.js
+++ src/Places.js
@@ -23,7 +23,7 @@ useEffect(() => {
     (p, id) => 
             <>
             <img src={"https://assets.atlasobscura.com/media/W1siZiIsInVwbG9hZHMvcGxhY2VfaW1hZ2VzLzJjNzliZDJkLWMzZDEtNDJkMy04YzRiLWIzZDM1NGRmN2JjNjMwYjVlYWU1YzJhYTI4NTM5M19naG9zdF9yYW5jaF8xLmpwZyJdLFsicCIsInRodW1iIiwiMTIwMHg-Il0sWyJwIiwiY29udmVydCIsIi1xdWFsaXR5IDgxIC1hdXRvLW9yaWVudCJdXQ/ghost_ranch_4.jpg"} alt={p.lat} height="777" width="777" />
-            <li key={id}>Latitude: {p.lat} Longitude: {p.lon} Url: <a href={`https://atlasobscura.com/places/${p.id}`} class="btn cta bg"target="blank">Atlas Obscura Entry</a>
+            <li key={id}>Latitude: {p.lat} Longitude: {p.lng} Url: <a href={`https://atlasobscura.com/places/${p.id}`} class="btn cta bg"target="blank">Atlas Obscura Entry</a>
             </li>
             </>
   );
